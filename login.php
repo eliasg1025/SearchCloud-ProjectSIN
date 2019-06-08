cbdecbf9219ef4e5d915ffcc3d8737ab60f0274c
Elias Guere <eliasguere1025@gmail.com>
2019-06-07 20:59:05 -0500

    Funcion de Login

--- login.php
+++ login.php
@@ -1,3 +1,27 @@
+<?php
+    session_start();
+
+    if (isset($_SESSION['idUsuario'])) {
+        header('Location: /Projects/SearchCloud-ProjectSIN/index.php');
+    }
+
+    require "database.php";
+
+    if (!empty($_POST['email']) && !empty($_POST['password'])) {
+        $records = $conn->prepare('SELECT `idUsuario`, `email`, `password` FROM `modelosin`.`usuario` WHERE `email` = :email;');
+        $records->bindParam(':email', $_POST['email']);
+        $records->execute();
+        $results = $records->fetch(PDO::FETCH_ASSOC);
+        $message = '';
+        if (count($results) > 0 && $results["password"]==$_POST["password"]) {
+            $_SESSION['user_id'] = $results['idUsuario'];
+            header("Location: /Projects/SearchCloud-ProjectSIN/main/index.php");
+        } else {
+            $message = 'Sorry, Email o contraseña incorrectos';
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,14 +42,25 @@
 	<?php require "partials/nouser-navbar.php" ?>
 
 	<div class="container login-container">
+
             <div class="login-form-1">
+
+                <?php if (!empty($message)): ?>
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <?= $message ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <?php endif; ?>
+
                 <h3>Ingresa</h3>
-                <form action="main.php" method=""> <!-- Cambiar -->
+                <form action="login.php" method="POST"> <!-- Cambiar -->
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Correo Electronico" value="" required>
+                        <input type="text" name="email" class="form-control" placeholder="Correo Electronico" value="" required>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Contraseña" value="" required>
+                        <input type="password" name="password" class="form-control" placeholder="Contraseña" value="" required>
                     </div>
                     <div class="form-group">
                         <input type="submit" class="btnSubmit" value="Login"/>
